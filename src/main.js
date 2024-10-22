1a274c4fa9603d85c067f5cd57aee0f8a5da1b53
omiya0555 <otoharadak@gmail.com>
2024-10-22 22:57:19 +0900

    Install plugins.Setup vue-project.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
+import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.mount('#app')
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.VITE_PUSHER_APP_KEY,
+    cluster: process.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
